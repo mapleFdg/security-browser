5f400cffdc289eb03fb666a3262c6f4269a6d54c
hzc <hzc@localhost>
2020-02-06 15:35:23 +0800

    增加session的存取类

---
+++ src/main/java/com/maple/security/browser/validate/code/impl/SessionValidateCodeRepository.java
@@ -0,0 +1,48 @@
+package com.maple.security.browser.validate.code.impl;
+
+import org.springframework.social.connect.web.HttpSessionSessionStrategy;
+import org.springframework.social.connect.web.SessionStrategy;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.ServletWebRequest;
+
+import com.maple.security.core.validate.code.ValidateCode;
+import com.maple.security.core.validate.code.ValidateCodeRepository;
+import com.maple.security.core.validate.code.ValidateCodeType;
+
+/**
+ * 
+ * 基于Session的检验码存取器
+ * 
+ * @author hzc
+ *
+ */
+@Component
+public class SessionValidateCodeRepository implements ValidateCodeRepository {
+
+	/**
+	 * 验证码放入session时的前缀
+	 */
+	private String SESSION_KEY_PREFIX = "SESSION_KEY_FOR_CODE_";
+
+	private SessionStrategy sessionStrategy = new HttpSessionSessionStrategy();
+
+	@Override
+	public void save(ServletWebRequest request, ValidateCode validateCode, ValidateCodeType validateCodeType) {
+		sessionStrategy.setAttribute(request, getSessionKey(request, validateCodeType), validateCode);
+	}
+
+	@Override
+	public ValidateCode get(ServletWebRequest request, ValidateCodeType validateCodeType) {
+		return (ValidateCode) sessionStrategy.getAttribute(request, getSessionKey(request, validateCodeType));
+	}
+
+	@Override
+	public void remove(ServletWebRequest request, ValidateCodeType validateCodeType) {
+		sessionStrategy.removeAttribute(request, getSessionKey(request, validateCodeType));
+	}
+
+	private String getSessionKey(ServletWebRequest request, ValidateCodeType validateCodeType) {
+		return SESSION_KEY_PREFIX + validateCodeType.toString().toUpperCase();
+	}
+
+}
