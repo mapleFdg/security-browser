a0f27078223f9dfa6cddac95be02a27e27424bee
hzc <hzc@localhost>
2020-02-08 16:35:53 +0800

    增加remember-me的配置

--- src/main/java/com/maple/security/browser/BrowserSecurityConfig.java
+++ src/main/java/com/maple/security/browser/BrowserSecurityConfig.java
@@ -1,15 +1,22 @@
 package com.maple.security.browser;
 
+import java.sql.Date;
+
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
 import com.maple.security.browser.authentication.MapleAuthenticationSuccessHandler;
 import com.maple.security.core.properties.SecurityConstants;
@@ -19,23 +26,62 @@ import com.maple.security.core.validate.code.ValidateCodeFilter;
 @Configuration
 public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
 	
+	/**
+	 * 系统配置
+	 */
 	@Autowired
 	private SecurityProperties securityProperties;
 	
-	
+	/**
+	 * 验证成功处理器
+	 */
 	@Autowired
 	private AuthenticationSuccessHandler mapleAuthenticationSuccessHandler;
 	
+	/**
+	 * 验证失败处理器
+	 */
 	@Autowired
 	private AuthenticationFailureHandler mapleAuthenticationFailureHandler;
 	
+	/**
+	 * 验证码校验过滤器
+	 */
 	@Autowired
 	private ValidateCodeFilter validateCodeFilter;
 	
-
+	/**
+	 * 数据源
+	 */
+	@Autowired
+	private DataSource dataSource;
+	
+	/**
+	 * 获取用户信息的类
+	 */
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	/**
+	 * 声明加密方法
+	 * @return
+	 */
 	@Bean
 	public PasswordEncoder getPasswordEncod() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	/**
+	 * 声明一个token的存取器
+	 * @return
+	 */
+	@Bean
+	public PersistentTokenRepository getPersistentTokenRepository() {
+		JdbcTokenRepositoryImpl tokenRepository = new JdbcTokenRepositoryImpl();
+		tokenRepository.setDataSource(dataSource);
+		// 配置是否在项目启动时创建表
+//		tokenRepository.setCreateTableOnStartup(true);
+		return tokenRepository;
 	}
 
 	@Override
@@ -47,14 +93,24 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
 				.loginProcessingUrl(SecurityConstants.DEFAULT_SIGN_IN_PROCESSING_URL_FORM)
 				.successHandler(mapleAuthenticationSuccessHandler)
 				.failureHandler(mapleAuthenticationFailureHandler)
-				.and()
+			.and()
+				.rememberMe()
+					.tokenRepository(getPersistentTokenRepository())
+					.tokenValiditySeconds(securityProperties.getBrowser().getRememberMeSeconds())
+					.userDetailsService(userDetailsService)
+			.and()
 				.authorizeRequests()
 				.antMatchers(
 						securityProperties.getBrowser().getLoginPage(), 
 						SecurityConstants.DEFAULT_UNAUTHENTICATION_URL,
-						"/code/image")
+						SecurityConstants.DEFAULT_VALIDATE_CODE_URL_PREFIX + "/*"
+					)
 				.permitAll()
-				.anyRequest().authenticated().and().csrf().disable();
+				.anyRequest()
+				.authenticated()
+			.and()
+				.csrf()
+				.disable();
 	}
 
 }
